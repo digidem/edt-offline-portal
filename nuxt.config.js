0eb9b12ee15157a85a1dfc0f932a6fdd45c91a2e
luandro <luandro@gmail.com>
2022-02-07 15:41:21 -0300

    feat: adiciona suite de lint

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,16 +18,20 @@ export default {
     }
   },
   buildModules: [
-    "nuxt-windicss",
-    "@nuxtjs/pwa",
-    "@nuxtjs/localforage",
-    "@nuxtjs/svg",
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/stylelint
+    '@nuxtjs/stylelint-module',
+    'nuxt-windicss',
+    '@nuxtjs/pwa',
+    '@nuxtjs/localforage',
+    '@nuxtjs/svg',
   ],
   components: true,
   build: {
     extend(config, ctx) { }, // blah blah
   },
   server: {
-    host: "0.0.0.0",
+    host: '0.0.0.0',
   },
-};
+}
