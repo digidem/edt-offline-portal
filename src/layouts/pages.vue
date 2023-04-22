59780a4880773cff10aaff5f4d92324810a53097
Luandro <luandro@digital-democracy.org>
2023-04-22 13:07:03 -0300

    chore: add language switch to items navbar

--- src/layouts/pages.vue
+++ src/layouts/pages.vue
@@ -8,4 +8,6 @@
   </div>
 </template>
 
-<script></script>
+<script>
+export default {};
+</script>
--- src/pages/guide-lines.vue
+++ src/pages/guide-lines.vue
@@ -1,18 +1,26 @@
 <template>
   <article>
-    <h2 class="text-3xl text-center py-10px">{{ page.title }}</h2>
-    <div class="w-90vw m-auto py-10px">
-      <nuxt-content :document="page" />
+    <div class="pt-48">
+      <h2 class="text-3xl text-center py-10px">{{ page.title }}</h2>
+      <div class="w-90vw m-auto py-10px">
+        <nuxt-content :document="page" />
+      </div>
     </div>
   </article>
 </template>
 
 <script>
 export default {
-  layout: "pages",
-  async asyncData({ $content }) {
-    const page = await $content("guide-lines").fetch();
-
+  layout: "items",
+  async asyncData({ $content, i18n }) {
+    const locale = i18n.getLocaleCookie();
+    const term = "guide-lines";
+    const localized = locale
+      ? locale === "en"
+        ? term
+        : `${term}_${locale}`
+      : term;
+    const page = await $content(localized).fetch();
     return {
       page,
     };
