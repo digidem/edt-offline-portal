9cccd9267f314e95f943738db126f6033a4e3a88
Luandro <luandro@digital-democracy.org>
2023-04-22 13:37:25 -0300

    fix: filder indexes from blocks

--- src/libs/getParsedBlocks.js
+++ src/libs/getParsedBlocks.js
@@ -1,6 +1,7 @@
 export default (pages, locale, root) => {
   const localizedBlocks = pages
     .filter((c) => {
+      if (c.slug?.includes("index")) return false;
       const fileName = c.path.split("/")[c.path.split("/").length - 1];
       const fileLocale = fileName.split("_")[1];
       if (!fileLocale && locale === "en") {
@@ -10,7 +11,6 @@ export default (pages, locale, root) => {
     })
     .sort((a, b) => a.slug.localeCompare(b.slug));
   if (root) {
-    console.log("localizedBlocks", localizedBlocks);
     return localizedBlocks;
   } else {
     const blocks = localizedBlocks.filter(
